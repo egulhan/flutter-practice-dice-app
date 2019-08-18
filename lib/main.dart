6ea5f11dcb5cdde04b96d6a1bf0d67a6db584ad2
Erman Gülhan <mr.ermangulhan@gmail.com>
2019-08-18 14:49:11 +0300

    - replace to stateful widget - use setState() func. - randomise dice numbers by custom function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,7 +16,22 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNum = 1;
+  int rightDiceNum = 1;
+
+  void randomiseDiceNumbers() {
+    setState(() {
+      leftDiceNum = Random().nextInt(6) + 1;
+      rightDiceNum = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -23,15 +39,19 @@ class DicePage extends StatelessWidget {
         child: Row(
           children: <Widget>[
             Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Image.asset('images/dice6.png'),
+              child: FlatButton(
+                onPressed: () {
+                  randomiseDiceNumbers();
+                },
+                child: Image.asset('images/dice$leftDiceNum.png'),
               ),
             ),
             Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Image.asset('images/dice6.png'),
+              child: FlatButton(
+                onPressed: () {
+                  randomiseDiceNumbers();
+                },
+                child: Image.asset('images/dice$rightDiceNum.png'),
               ),
             ),
           ],
